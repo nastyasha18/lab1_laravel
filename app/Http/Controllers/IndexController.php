3ceb7609d1298d2ec3bb6990e69397da1fac6ea0
nastyasha18 <131899204+nastyasha18@users.noreply.github.com>
2024-11-20 21:17:52 +0500

    added route and controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function getPhpInfo() {
+        return response()->json([
+            'currentPhpVersion' => phpversion()
+        ]);
+    }
+
+    public function getClientInfo() {
+        return response()->json([
+            'ip' => request()->ip(),
+            'userAgent' => request()->userAgent()
+        ]);
+    }
+
+    public function getDatabaseInfo() {
+        return response()->json([
+            'database' => DB::connection()->getDatabaseName()
+        ]);
+    }
+}
